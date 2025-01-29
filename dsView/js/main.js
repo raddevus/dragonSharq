c97f54ecd3a812cb31ae00d1778cfc5aab824d1e
raddevus <1radeutsch1@gmail.com>
2025-01-29 10:28:34 -0500

    Now user can retrieve the html, save it in a file - run from main project folder (ex. dotnet run --project draqgonSharq https://newlibre.com -file dsView/index.htm && cd dsView && npm start)  That will retrieve from the URL, save it in the dsView index.htm and start npm (electron browser)

--- dsView/js/main.js
+++ dsView/js/main.js
@@ -1,3 +1,28 @@
 // main.js
 
-console.log(`${new Date()} I'm running!`);
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.htm')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
