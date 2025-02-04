35c1627282fad0585312ffea28e6c2b3021a7937
raddevus <1radeutsch1@gmail.com>
2025-02-04 11:46:41 -0500

    Adding custom menu so I can add back functionality.  This is a work-in-progress (wip)

--- dsView/js/main.js
+++ dsView/js/main.js
@@ -1,11 +1,12 @@
 // main.js
 
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, Menu } = require('electron/main')
 const args = process.argv;
 console.log(`${args.length} : ${args}`);
 
 const jsIsOn = args[2];
 console.log(`jsIsOn: ${jsIsOn}`);
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -20,8 +21,38 @@ const createWindow = () => {
       }
     
   })
+  const { navigationHistory } = win.webContents;
   win.loadFile('index.htm');
   console.log(`zoomFactor: ${win.webContents.zoomFactor}`);
+  // Create the application menu
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Yada',
+      submenu: [
+        { label: 'Open', click: () => { console.log('Open clicked'); } },
+        { label: 'Save', click: () => { console.log('Save clicked'); } },
+        { label: 'Back', click: () => {navigationHistory.goBack()}},
+        { type: 'separator' },
+        { label: 'Exit', click: () => { app.quit(); } },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', role: 'undo' },
+        { label: 'Redo', role: 'redo' },
+        { label: 'Zoom In', role: 'zoomIn' },
+        { label: 'Zoom Out', role: 'zoomOut' },
+        { label: 'Reset Zoom', role: 'resetZoom' },
+        { type: 'separator' },
+        { label: 'Cut', role: 'cut' },
+        { label: 'Copy', role: 'copy' },
+        { label: 'Paste', role: 'paste' },
+      ],
+    },
+  ]);
+
+  Menu.setApplicationMenu(menu);
   
 }
 
